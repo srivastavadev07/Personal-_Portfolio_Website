961f48aa53b8d3957e52f352464015cbc1f294a3
Devansh Srivastava <srivastavadev2007@gmail.com>
2026-06-30 11:36:35 +0530

    Portfolio ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Personal-_Portfolio_Website /",
+  base: "/Personal-_Portfolio_Website/",
 });
